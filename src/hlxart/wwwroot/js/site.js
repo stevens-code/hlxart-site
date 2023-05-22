953b49732b649e268cb42f664ae552a76675b594
stevens-code <120059830+stevens-code@users.noreply.github.com>
2023-05-22 13:22:34 -0600

    Animate progress bars

--- src/hlxart/wwwroot/js/site.js
+++ src/hlxart/wwwroot/js/site.js
@@ -1,1 +1,6 @@
-﻿
+﻿function setProgress(progressSelector, percent) {
+    let element = document.querySelector(`${progressSelector} > .progress-bar`);
+    element.setAttribute("style", `width: ${percent}%`);
+    element.setAttribute("aria-valuenow", `${percent}`);
+    element.textContent = `${percent}%`;
+}
